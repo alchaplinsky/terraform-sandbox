24bbd98cc6f277a622fa3af36ad798186602e284
Alex Chaplinsky <alchaplinsky@gmail.com>
2022-08-08 23:38:51 +0200

    Add internet gateway and subnets

--- main.tf
+++ main.tf
@@ -18,3 +18,29 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
