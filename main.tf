67e9caeea9f79a25a76460dcd6d80051159dbe3d
Alex Chaplinsky <alchaplinsky@gmail.com>
2022-08-09 00:21:17 +0200

    Add internet gateway and routing table

--- main.tf
+++ main.tf
@@ -19,17 +19,18 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "public" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main"
+    Name = "public"
   }
 }
 
 resource "aws_subnet" "public-subnet-1" {
-  vpc_id     = "${aws_vpc.main.id}"
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+
 
   tags = {
     Name = "public-subnet-1"
@@ -37,10 +38,30 @@ resource "aws_subnet" "public-subnet-1" {
 }
 
 resource "aws_subnet" "private-subnet-1" {
-  vpc_id     = "${aws_vpc.main.id}"
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "private-subnet-1"
   }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route" "route" {
+  route_table_id = aws_route_table.public-route-table.id
+  destination_cidr_block     = "0.0.0.0/0"
+  gateway_id     = aws_internet_gateway.public.id
+  depends_on     = [aws_route_table.public-route-table]
 }
